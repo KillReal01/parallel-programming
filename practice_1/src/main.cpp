df0e951772f76728d6b5268d1d323ba32b9453f1
KillReal01 <kirill.bereza.02@mail.ru>
2025-10-12 18:37:13 +0300

    Initial commit

--- practice_1/src/main.cpp
+++ practice_1/src/main.cpp
@@ -15,10 +15,10 @@ int main()
     std::vector<std::string> files;
     std::string path = "input_text";
 
-    for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
-        if (entry.is_regular_file())
-            files.push_back(entry.path().string());
-    }
+    // for (const auto& entry : std::filemsystem::recursive_directory_iterator(path)) {
+    //     if (entry.is_regular_file())
+    //         files.push_back(entry.path().string());
+    // }
 
     // вывод для проверки
     for (const auto& f : files)
