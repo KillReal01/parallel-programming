a133705777f3941fb8d90369a705810fa5bd560f
KillReal01 <kirill.bereza.02@mail.ru>
2025-10-20 14:35:58 +0300

    update 1.4

--- practice_2/practice_2.4/src/main.cpp
+++ practice_2/practice_2.4/src/main.cpp
@@ -1,13 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <thread>
-#include <chrono>
-#include <mutex>
+/*
+    Course: Parallel Programming
+    Assignment: 2.4
+    Completed by: Bereza Kirill
+*/
 
+
+#include <iostream>
+#include <thread>
+#include <syncstream>
+
+
+void printString(int threadNumber)
+{
+    thread_local std::string s("hello from ");
+    s += std::to_string(threadNumber);
+
+    std::osyncstream stream(std::cout);
+    stream << "Address: " << &s << " string: " << s << std::endl;
+}
 
 int main()
 {
-    
+    const int num_threads = 4;
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < num_threads; ++i)
+        threads.emplace_back(&printString, i + 1);
+
+    for (auto& t : threads)
+        t.join();
 
     std::cin.get();
     return 0;
