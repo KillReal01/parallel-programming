cff44b6bcf3cabc26b5bb37c54b11fdf021c3842
KillReal01 <kirill.bereza.02@mail.ru>
2025-10-26 15:03:29 +0300

    update 3.5

--- practice_3/practice_3.5/practice_3.5.cpp
+++ practice_3/practice_3.5/practice_3.5.cpp
@@ -12,43 +12,42 @@
 #include <vector>
 #include <future>
 #include <stop_token>
-#include <syncstream>
 #include <functional>
-#include <chrono>
 #include <numeric>
 #include <iomanip>
-#include <cmath>
+#include <sstream>
+#include <syncstream>
 
 
 class ThreadPool
 {
 public:
     using task_type = std::packaged_task<int64_t()>;
-    // using task_type = std::function<void()>;
 
-    ThreadPool(size_t worker_number = std::max(1u, std::thread::hardware_concurrency()))
+    explicit ThreadPool(size_t worker_number = std::max(1u, std::thread::hardware_concurrency()))
     {
-        for (size_t i = 0; i < worker_number; i++)
+        _workers.reserve(worker_number);
+        for (size_t i = 0; i < worker_number; ++i)
             _workers.emplace_back(&ThreadPool::thread_loop, this, _ss.get_token());
     }
 
     ~ThreadPool()
     {
-        this->stop();
+        stop();
         for (auto& th : _workers)
-        {
             if (th.joinable())
                 th.join();
-        }
     }
 
-    void append_task(task_type task)
+    std::future<int64_t> append_task(task_type task)
     {
+        std::future<int64_t> future = task.get_future();
         {
             std::lock_guard lock(_mtx);
             _tasks.push(std::move(task));
         }
         _cv.notify_one();
+        return future;
     }
 
     void stop()
@@ -60,30 +59,30 @@ public:
 private:
     void thread_loop(std::stop_token st)
     {
-        thread_local int task_count = 0;
-        std::osyncstream os(std::cout);
+        std::osyncstream(std::cout) << "[ThreadPool] Id: " << std::this_thread::get_id() << "\tstarted\n";
 
-        // os << "Id: " << std::this_thread::get_id() << " started" << std::endl;
+        thread_local int number_tasks = 0;
         while (true)
         {
             task_type task;
             {
                 std::unique_lock lock(_mtx);
-                _cv.wait(lock, [this]() { return !_tasks.empty() || _ss.stop_requested(); });
+                _cv.wait(lock, [this, &st]() { return !_tasks.empty() || st.stop_requested(); });
 
-                if (_tasks.empty() && _ss.stop_requested())
-                    break;
+                if (_tasks.empty() && st.stop_requested())
+                {
+                    std::osyncstream(std::cout) << "[ThreadPool] Id: " << std::this_thread::get_id() << "\tfinished " << number_tasks << " tasks\n";
+                    return;
+                }
 
                 task = std::move(_tasks.front());
                 _tasks.pop();
             }
             task();
-            task_count++;
+            number_tasks++;
         }
-        // os << "Id: " << std::this_thread::get_id() << " finished " << task_count << " tasks" << std::endl;
     }
 
-private:
     std::vector<std::thread> _workers;
     std::queue<task_type> _tasks;
     std::stop_source _ss;
@@ -114,115 +113,131 @@ std::string formatDuration(const std::chrono::nanoseconds& time)
     return ss.str();
 }
 
-//void task1()
-//{
-//    constexpr size_t size = 100'000'000;
-//    std::vector<int> data(size);
-//    std::iota(data.begin(), data.end(), 1);
-//    long long sum = 0;
-//    for (auto v : data)
-//        sum += v;
-//}
-//
-//void task2()
-//{
-//    constexpr size_t N = 1000;
-//    std::vector<std::vector<double>> A(N, std::vector<double>(N, 1.0));
-//    std::vector<std::vector<double>> B(N, std::vector<double>(N, 2.0));
-//    std::vector<std::vector<double>> C(N, std::vector<double>(N, 0.0));
-//
-//    for (size_t i = 0; i < N; ++i)
-//        for (size_t j = 0; j < N; ++j)
-//            for (size_t k = 0; k < N; ++k)
-//                C[i][j] += A[i][k] * B[k][j];
-//}
-//
-//unsigned long long fib(int n)
-//{
-//    if (n <= 2) return 1;
-//    return fib(n - 1) + fib(n - 2);
-//}
-//
-//void task3()
-//{
-//    volatile auto r = fib(35);
-//}
-
-int64_t sumArray()
+int64_t sumArray(size_t size)
 {
-    constexpr size_t size = 100'000'000;
+    std::osyncstream(std::cout) << "Id: " << std::this_thread::get_id() << " start sumArray\n";
+
     std::vector<int> data(size);
     std::iota(data.begin(), data.end(), 1);
-
     int64_t sum = 0;
     for (auto v : data)
         sum += v;
 
+    std::osyncstream(std::cout) << "Id: " << std::this_thread::get_id() << " finished sumArray\n";
     return sum;
 }
 
-int64_t matrixDeterminantSum()
+int64_t matrixSum(size_t N)
 {
-    constexpr size_t N = 100;
+    std::osyncstream(std::cout) << "Id: " << std::this_thread::get_id() << " start matrixSum\n";
+
     std::vector<std::vector<int>> A(N, std::vector<int>(N, 1));
     std::vector<std::vector<int>> B(N, std::vector<int>(N, 2));
     std::vector<std::vector<int>> C(N, std::vector<int>(N, 0));
-
     for (size_t i = 0; i < N; ++i)
         for (size_t j = 0; j < N; ++j)
             for (size_t k = 0; k < N; ++k)
                 C[i][j] += A[i][k] * B[k][j];
-
     int64_t sum = 0;
     for (const auto& row : C)
         for (auto v : row)
             sum += v;
 
+    std::osyncstream(std::cout) << "Id: " << std::this_thread::get_id() << " finished matrixSum\n";
     return sum;
 }
 
-int64_t fibNumber(int n = 35)
+int64_t fibNumber(int N)
 {
-    if (n <= 2) return 1;
-    return fibNumber(n - 1) + fibNumber(n - 2);
+    if (N <= 2) return 1;
+    return fibNumber(N - 1) + fibNumber(N - 2);
+}
+
+void startThreadPool(int task_number, const std::vector<std::function<int64_t()>>& pool_tasks)
+{
+    std::vector<std::future<int64_t>> futures;
+    futures.reserve(task_number);
+
+    auto start = std::chrono::steady_clock::now();
+    {
+        ThreadPool pool;
+        for (int i = 0; i < task_number; ++i)
+        {
+            std::packaged_task<int64_t()> task(pool_tasks[i % pool_tasks.size()]);
+            futures.push_back(pool.append_task(std::move(task)));
+        }
+        for (auto& f : futures)
+            f.get();
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "ThreadPool time: " << formatDuration(end - start) << std::endl;
+}
+
+void startParallel(int task_number, const std::vector<std::function<int64_t()>>& pool_tasks)
+{
+    std::vector<std::future<int64_t>> futures;
+    futures.reserve(task_number);
+    std::vector<std::thread> threads;
+    threads.reserve(task_number);
+
+    auto start = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < task_number; ++i)
+    {
+        std::packaged_task<int64_t()> task(pool_tasks[i % pool_tasks.size()]);
+        futures.push_back(task.get_future());
+        threads.emplace_back(std::move(task));
+    }
+    for (auto& t : threads)
+        t.join();
+    for (auto& f : futures)
+        f.get();
+
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "Individual threads time: " << formatDuration(end - start) << std::endl;
 }
 
 int main()
 {
-    int task_number = 100;
-    std::vector<std::function<int64_t()>> pool_tasks{ sumArray, matrixDeterminantSum, fibNumber };
-
-    std::vector<std::function<void()>> tasks;
-    tasks.reserve(task_number);
-
-    for (int i = 0; i < task_number; i++)
-        tasks.emplace_back(pool_tasks[i % pool_tasks.size()]);
-
-    // Thread pool
-    auto start = std::chrono::steady_clock::now();
+    int task_number = 200;
+    std::vector<std::function<int64_t()>> pool_tasks
     {
-        ThreadPool pool;
-        for (const auto& task : tasks)
-            pool.append_task(task);
-    }
-    auto end = std::chrono::steady_clock::now();
-    std::cout << "Thread pool time: " << formatDuration(end - start) << std::endl;
+        []() { return sumArray(10'000'000); },
+        []() { return matrixSum(1000); },
+        []() { return fibNumber(40); }
+    };
+
+    // ThreadPool
+    startThreadPool(task_number, pool_tasks);
 
     // Individual threads
-    start = std::chrono::steady_clock::now();
-    {
-        std::vector<std::thread> threads;
-        for (const auto& task : tasks)
-            threads.emplace_back(task);
-        for (auto& t : threads)
-            t.join();
-    }
-    end = std::chrono::steady_clock::now();
-    std::cout << "Individual threads time: " << formatDuration(end - start) << std::endl;
+    startParallel(task_number, pool_tasks);
 
     return 0;
 }
 
-// На тяжелых операциях победу одержал ThreadPool
-// Thread pool time : 00h : 00m : 21s.486ms.771mks.700ns
-// Individual threads time : 00h : 00m : 25s.343ms.817mks.800ns
+// С небольшим отрывом победу одержал ThreadPool
+
+//[ThreadPool] Id: 7300   started
+//[ThreadPool] Id : 17972  started
+//[ThreadPool] Id : 21024  started
+//[ThreadPool] Id : 11468  started
+//[ThreadPool] Id : 1644   started
+//[ThreadPool] Id : 21084  started
+//[ThreadPool] Id : 18332  started
+//[ThreadPool] Id : 21128  started
+//[ThreadPool] Id : 3784   started
+//[ThreadPool] Id : 21024  finished 18 tasks
+//[ThreadPool] Id : 21044  finished 15 tasks
+//[ThreadPool] Id : 18332  finished 16 tasks
+//[ThreadPool] Id : 21128  finished 15 tasks
+//[ThreadPool] Id : 19212  finished 15 tasks
+//[ThreadPool] Id : 3784   finished 21 tasks
+//[ThreadPool] Id : 17972  finished 13 tasks
+//[ThreadPool] Id : 21084  finished 21 tasks
+//[ThreadPool] Id : 7300   finished 21 tasks
+//[ThreadPool] Id : 11468  finished 11 tasks
+//[ThreadPool] Id : 1644   finished 17 tasks
+//[ThreadPool] Id : 15112  finished 17 tasks
+//ThreadPool time : 00h : 00m : 32s.884ms.092mks.900ns
+//Individual threads time : 00h : 00m : 35s.313ms.520mks.400ns
