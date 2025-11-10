04e76d6362073bc3611dc1c296c54138f460f6f7
kirill.bereza <kirill.bereza@gs-labs.ru>
2025-11-10 16:58:22 +0300

    update 4.3

--- practice_4/practice_4.3/practice_4.3.cpp
+++ practice_4/practice_4.3/practice_4.3.cpp
@@ -10,11 +10,10 @@
 #include <atomic>
 #include <barrier>
 #include <climits>
-#include <array>
+#include <algorithm>
 
 
-// Условие не совсем ясно, понял как:
-// Найти минимальный элемент на каждой позиции среди всех векторов и вывести массив этих минимумов.
+constexpr int window_size = 5;
 
 
 int main()
@@ -26,28 +25,39 @@ int main()
         {-5, 3, 4, 33, 22}
     };
 
-    const int count = 5;
-    std::array<std::atomic<int>, count> min_values;
+    auto it = std::ranges::max_element(vv, {}, [](const auto &v){ return v.size(); });
+    size_t max_len = (it != vv.end()) ? it->size() : 0;
 
-    for (int i = 0; i < count; ++i)
-        min_values[i].store(vv.front()[i]);
+    size_t windows_count = max_len / window_size + ((max_len % window_size) ? 1 : 0);
+
+    std::atomic<int> current_min{INT_MAX};
 
     std::barrier barrier(vv.size(), [&]() noexcept {
-        std::cout << "Minimums: ";
-        for (int i = 0; i < count; ++i)
-            std::cout << min_values[i] << " ";
-        std::cout << std::endl;
-        });
+        std::cout << "Minimum of current window: " << current_min.load() << std::endl;
+        current_min.store(INT_MAX, std::memory_order_relaxed);
+    });
 
     auto worker = [&](const std::vector<int>& v) {
-        for (int i = 0; i < count && i < v.size(); ++i)
+        for (size_t w = 0; w < windows_count; ++w)
         {
-            int current = min_values[i].load(std::memory_order_relaxed);
-            while (v[i] < current && !min_values[i].compare_exchange_weak(current, v[i],
-                                                                          std::memory_order_release,
-                                                                          std::memory_order_relaxed));
+            size_t start = w * window_size;
+            size_t end = std::min(start + window_size, v.size());
+            if (start >= v.size())
+            {
+                barrier.arrive_and_wait();
+                continue;
+            }
+
+            int window_min = *std::min_element(v.begin() + start, v.begin() + end);
+            int prev = current_min.load(std::memory_order_relaxed);
+
+            while (window_min < prev &&
+                   !current_min.compare_exchange_weak(prev, window_min,
+                                                      std::memory_order_release,
+                                                      std::memory_order_relaxed));
+
+            barrier.arrive_and_wait();
         }
-        barrier.arrive_and_wait();
     };
 
     std::vector<std::thread> threads;
